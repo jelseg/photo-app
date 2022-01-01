19602fb40f4887a0845c8ca935185ca5f769334c
Jelle is awesome <segers.jelle@hotmail.com>
2022-01-01 16:16:29 +0100

    fixed sidebar
    working collapsable sidebar
    (still needs to ad links)

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -17,28 +17,21 @@ import "../stylesheets/application"
 
 
 //sidebar
+/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
+//var element = document.getElementById("openNavBtn");
+//element.on("click", function() {
+window.openNav = function() {
+  document.getElementById("mySidebar").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
 
-$(document).ready(function () {
-    var trigger = $('.hamburger'),
-        overlay = $('.overlay'),
-       isClosed = false;
-      trigger.click(function () {
-        hamburger_cross();      
-      });
-      function hamburger_cross() {
-        if (isClosed == true) {          
-          overlay.hide();
-          trigger.removeClass('is-open');
-          trigger.addClass('is-closed');
-          isClosed = false;
-        } else {   
-          overlay.show();
-          trigger.removeClass('is-closed');
-          trigger.addClass('is-open');
-          isClosed = true;
-        }
-    }
-    $('[data-toggle="offcanvas"]').click(function () {
-          $('#wrapper').toggleClass('toggled');
-    });  
-  });
+  document.getElementById("openSidebar").style.display = "none";
+}
+
+
+/* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+window.closeNav = function() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+
+  document.getElementById("openSidebar").style.display = "block";
+}
